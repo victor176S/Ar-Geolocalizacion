519c4e07ed488ff049ea9165a9934429cc6eb25a
KR13 <kilianvidal02@gmail.com>
2026-02-25 10:19:36 +0100

    Cambiado el cofre

--- Assets/Scripts/SlipperyKey.cs
+++ Assets/Scripts/SlipperyKey.cs
@@ -10,11 +10,8 @@ public class SlipperyKey : MonoBehaviour
 
     public Button llave;
 
-    public GameObject tapaCofre;
+    public GameObject cofre;
 
-    public float velocidadBase = 200f;
-    private Vector2 direccion;
-    public RectTransform canvasRect;
     private bool yendoHaciaIzq;
     private bool yendoHaciaArriba;
 
@@ -22,58 +19,13 @@ public class SlipperyKey : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        direccion = Random.insideUnitCircle.normalized;
-
-        
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        StartCoroutine(MovimientoLLave());
-        
-        // if (vecesPulsadas > 0 && vecesPulsadas < 20)
-        // {
-        //     float velocidad = velocidadBase + (vecesPulsadas * 20f);
-
-        //     canvasRect.anchoredPosition += direccion * velocidad * Time.deltaTime;
-
-        //     Vector2 pos = canvasRect.anchoredPosition;
-
-        //     float limiteX = 860f;
-        //     float limiteY = 440f;
-
-        //     if (pos.x > limiteX || pos.x < -limiteX)
-        //         direccion.x *= -1;
-
-        //     if (vecesPulsadas >= 5)
-        //     {
-        //         if (pos.y > limiteY || pos.y < -limiteY)
-        //             direccion.y *= -1;
-        //     }
-
-        //     llave.transform.position += (Vector3)(direccion * velocidad * Time.deltaTime);
-
-        //     Vector3 pos = llave.transform.position;
-
-        //     // Límites X
-        //     if (pos.x > 860 || pos.x < -860)
-        //     {
-        //         direccion.x *= -1;
-        //     }
-
-        //     // Límites Y (solo después de 5 pulsaciones)
-        //     if (vecesPulsadas >= 5)
-        //     {
-        //         if (pos.y > 440 || pos.y < -440)
-        //         {
-        //             direccion.y *= -1;
-        //         }
-        //     }
-        // }
-
-        
+        StartCoroutine(MovimientoLLave());       
     }
 
     IEnumerator MovimientoLLave()
@@ -172,21 +124,13 @@ public class SlipperyKey : MonoBehaviour
                 {
 
                 llave.gameObject.SetActive(false);
-
-                if (tapaCofre.gameObject.transform.localEulerAngles.x < 140)
-                {
-                    tapaCofre.gameObject.transform.localRotation = Quaternion.Euler(0.5f + tapaCofre.gameObject.transform.localEulerAngles.x, 0,0);
-                }
-
-                if(tapaCofre.gameObject.transform.localEulerAngles.x >= 140)
-                {
-                
-                    SceneManager.LoadScene(2);
+                Instantiate(cofre);
+                // cofre.AbrirTapa();
 
                 }
             }
 
-            }
+            
         
 
         
